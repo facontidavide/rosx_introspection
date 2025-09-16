0233fd501692121a9f77181a2ee37400181ce26e
Davide Faconti <davide.faconti@gmail.com>
2025-09-16 11:56:51 +0200

    fix memory and add README

--- python/rosx_introspection_py.cpp
+++ python/rosx_introspection_py.cpp
@@ -9,12 +9,42 @@
 
 namespace details
 {
-int convertToMsgpack(const RosMsgParser::FlatMessage& flat_msg, uint8_t* buffer)
+void convertToMsgpack(const RosMsgParser::FlatMessage& flat_msg,
+                      std::vector<uint8_t>& msgpack_data)
 {
+  msgpack_data.clear();
+  msgpack_data.resize(1024 * 64);  // 64KB initial size
+
+  auto resize_if_needed = [&msgpack_data](size_t required_size) {
+    if (msgpack_data.size() < required_size)
+    {
+      // Resize the buffer if it's not large enough
+      msgpack_data.resize(msgpack_data.size() * 2);
+    }
+  };
+
   const uint32_t num_elements = flat_msg.value.size() + flat_msg.name.size();
-  uint8_t* data_ptr = buffer;
+  uint8_t* data_ptr = msgpack_data.data();
 
   data_ptr += msgpack::pack_map(data_ptr, num_elements);
+
+  auto pack_variant = [](const RosMsgParser::Variant& number,
+                         uint8_t*& data) -> uint32_t {
+    switch (number.getTypeID())
+    {
+      case RosMsgParser::BuiltinType::UINT64:
+        return msgpack::pack_uint(data, number.extract<uint64_t>());
+      case RosMsgParser::BuiltinType::FLOAT64:
+        return msgpack::pack_double(data, number.extract<double>());
+      case RosMsgParser::BuiltinType::FLOAT32:
+        return msgpack::pack_float(data, number.extract<float>());
+      case RosMsgParser::BuiltinType::BOOL:
+        return msgpack::pack_bool(data, number.extract<bool>());
+      default:
+        // fallback to int64 for all the other types
+        return msgpack::pack_int(data, number.convert<int64_t>());
+    }
+  };
 
   std::string key_str;
 
@@ -22,38 +52,21 @@ int convertToMsgpack(const RosMsgParser::FlatMessage& flat_msg, uint8_t* buffer)
   for (const auto& [key, num_value] : flat_msg.value)
   {
     key.toStr(key_str);
+    resize_if_needed(4 + key_str.size() + 9);  // max key + max value size
     data_ptr += msgpack::pack_string(data_ptr, key_str);
-
-    switch (num_value.getTypeID())
-    {
-      case RosMsgParser::BuiltinType::UINT64:
-        data_ptr += msgpack::pack_uint(data_ptr, num_value.extract<uint64_t>());
-        break;
-      case RosMsgParser::BuiltinType::FLOAT64:
-        data_ptr += msgpack::pack_double(data_ptr, num_value.extract<double>());
-        break;
-      case RosMsgParser::BuiltinType::FLOAT32:
-        data_ptr += msgpack::pack_float(data_ptr, num_value.extract<float>());
-        break;
-      case RosMsgParser::BuiltinType::BOOL:
-        data_ptr += msgpack::pack_bool(data_ptr, num_value.extract<bool>());
-        break;
-      default:
-        // fallback to int64 for all the other types
-        data_ptr += msgpack::pack_int(data_ptr, num_value.convert<int64_t>());
-        break;
-    }
+    data_ptr += pack_variant(num_value, data_ptr);
   }
 
   // Write string values as key-value pairs
   for (const auto& [key, string_value] : flat_msg.name)
   {
     key.toStr(key_str);
+    resize_if_needed(8 + key_str.size() + string_value.size());
     data_ptr += msgpack::pack_string(data_ptr, key_str);
     data_ptr += msgpack::pack_string(data_ptr, string_value);
   }
 
-  return static_cast<int>(data_ptr - buffer);
+  msgpack_data.resize(static_cast<int>(data_ptr - msgpack_data.data()));
 }
 }  // namespace details
 
@@ -97,8 +110,9 @@ public:
       throw std::runtime_error("Failed to parse ROS message");
     }
 
-    const int msgpack_size = details::convertToMsgpack(flat_msg_, output_buffer_.data());
-    return nb::bytes(reinterpret_cast<const char*>(output_buffer_.data()), msgpack_size);
+    details::convertToMsgpack(flat_msg_, output_buffer_);
+    return nb::bytes(reinterpret_cast<const char*>(output_buffer_.data()),
+                     output_buffer_.size());
   }
 };
 
